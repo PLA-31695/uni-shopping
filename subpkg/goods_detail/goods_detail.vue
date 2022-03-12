961c65287c8475a3ef8d729c66cfe6d927f6dfe5
圣剑骑士团 <jhg1146069766@163.com>
2022-03-12 14:47:40 +0800

    商品详情页finished

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,40 @@
 <template>
 	<view>
-		goods_detail
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" style="height: 750rpx;">
+			<swiper-item v-for="(item,index) in goods_detail.pics" :key="index">
+				<view class="swiper-item" @click="preview(index)">
+					<image :src="item.pics_big" mode="widthFix"></image>
+				</view>
+			</swiper-item>
+		</swiper>
+		
+		<!-- 信息 -->
+		<view class="goods_detail_info" v-if="goods_detail.goods_price">
+			<view class="price">
+				￥{{goods_detail.goods_price}}
+			</view>
+			<view class="goods_name">
+				<view class="goods_name_left">
+					{{goods_detail.goods_name}}
+				</view>
+				<view class="goods_name_right">
+					<uni-icons type="star" size="18"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<view class="info_bottom">
+				<text>快递：免运费</text>
+			</view>
+		</view>
+		
+		<!-- 详情信息 -->
+		<rich-text :nodes="goods_detail.goods_introduce"></rich-text>
+		
+		<!-- 导航组件 -->
+		<view class="goods_nav">
+			<uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
@@ -8,12 +42,107 @@
 	export default {
 		data() {
 			return {
-				
+				goods_id:'',
+				goods_detail:{},
+				   options: [{
+							icon: 'shop',
+							text: '店铺',
+							infoColor:"red"
+						}, {
+							icon: 'cart',
+							text: '购物车',
+							info: 2
+						}],
+						  buttonGroup: [{
+							      text: '加入购物车',
+							      backgroundColor: '#ff0000',
+							      color: '#fff',
+								  
+							    },
+							    {
+							      text: '立即购买',
+							      backgroundColor: '#ffa200',
+							      color: '#fff'
+							    }
+							    ]
 			};
+		},
+		onLoad(options){
+			console.log(options)
+			this.goods_id = options.goods_id
+			this.getGoodsDetail()
+		},
+		methods:{
+			async getGoodsDetail(id){
+				const {data:res} = await uni.$http.get('/api/public/v1/goods/detail?goods_id=' + this.goods_id)
+				if(res.meta.status !== 200){
+					uni.showToast({
+						title:"请求失败",
+						icon:"error",
+						duration:1500
+					})
+				}else{
+					this.goods_detail = res.message
+				}
+				
+			},
+			preview(index){
+				uni.previewImage({
+					current:index,
+					urls:this.goods_detail.pics.map(item => item.pics_big)
+				})
+			},
+			
+			onClick(e){
+				console.log(e)
+				if(e.content.text == "购物车"){
+					uni.switchTab({
+						url:'../../pages/carts/carts'
+					})
+				}
+			}
+			
 		}
 	}
 </script>
 
 <style lang="scss">
+.swiper-item{
+	image{
+		width: 100%;
+	}
+}
 
+//info
+.goods_detail_info{
+	padding: 10px;
+	.price{
+		color: red;
+		font-size: 18px;
+	}
+	.goods_name{
+		display: flex;
+		justify-content: space-between;
+		.goods_name_left{
+			padding-right: 20px;
+		}
+		.goods_name_right{
+			width: 120rpx;
+			font-size: 12px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			color: gray;
+		}
+	}
+	.info_bottom{
+		color: gray;
+		font-size: 12px;
+		margin: 10px 0;
+	}
+}
+.goods_nav{
+	position: sticky;
+	bottom:0;
+}
 </style>
